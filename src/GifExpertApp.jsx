6c526b0e752862ba55f4b02f05c23d3213fe952a
sol <solagustinaandriani@gmail.com>
2025-01-20 12:55:51 -0300

    index.htmal actualizado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,38 +3,39 @@
 
 
 import { useState } from 'react';
-import { AddCategory, GifGrid } from './Components';  // Asegúrate de que no haya errores tipográficos o rutas incorrectas
-
+import { AddCategory, GifGrid } from '../components';
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState(['Sol']);
-
-    const onAddCategory = (newCategory) => {
-        if (categories.includes(newCategory)) return; // Evita duplicados
-
-        console.log(newCategory);
-        setCategories([newCategory, ...categories]); // Corrige el uso del estado
-    };
+    
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
+    
+    const onAddCategory = ( newCategory ) => {
+        if ( categories.includes(newCategory) ) return;
+        setCategories([ newCategory, ...categories ]);
+    }
+    
 
     return (
         <>
-          
+
             <h1>GifExpertApp</h1>
 
-     
+    
             <AddCategory 
-                onNewCategory={(value) => onAddCategory(value)} 
+                onNewCategory={ (value) => onAddCategory(value) }
             />
 
-                {
-                
-                categories.map((category) => (
+            { 
+                categories.map( ( category ) => (
                     <GifGrid 
-                    key={ category }
-                    category={ category }
-                    />
+                        key={ category } 
+                        category={ category } />
                 ))
-                }
+            }
+
+
+
+
         </>
-    );
-};
+    )
+}
